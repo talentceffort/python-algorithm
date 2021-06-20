a1761d59bf07ff99d279e8e55431f6c83d3d8686
talentceffort <talentceffort@gmail.com>
2021-06-20 11:42:44 +0900

    날짜계산 1476

---
+++ baekjoon/1000/1476.py
@@ -0,0 +1,29 @@
+from sys import stdin
+
+stdin = open("1476.txt")
+
+E, S, M = map(int, stdin.readline().split())
+
+i = 1
+
+e, s, m = 1, 1, 1
+
+while True:
+    if e == E and s == S and m == M:
+        print(i)
+        break
+
+    e += 1
+    s += 1
+    m += 1
+
+    if e == 16:
+        e = 1
+
+    if s == 29:
+        s = 1
+
+    if m == 20:
+        m = 1
+
+    i += 1
