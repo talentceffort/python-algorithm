9b47b9d2c238a5cbeea5c5e7df98c75fe374614b
talentceffort <talentceffort@gmail.com>
2021-07-15 16:36:42 +0900

    14889 스타트와 링크

---
+++ baekjoon/14000/14889.py
@@ -0,0 +1,51 @@
+from sys import stdin
+
+stdin = open("14889.txt")
+
+N = int(stdin.readline())
+
+s = [list(map(int, stdin.readline().split())) for _ in range(N)]
+
+a_min = 0
+
+def go(index, first, second):
+    if index == N:
+        if len(first) == 0:
+            return -1
+
+        if len(second) == 0:
+            return -1
+
+        t1 = 0
+        t2 = 0
+
+        for p1 in first:
+            for p2 in first:
+                if p1 == p2:
+                    continue
+                t1 += s[p1][p2]
+
+        for p1 in second:
+            for p2 in second:
+                if p1 == p2:
+                    continue
+                t2 += s[p1][p2]
+
+        diff = abs(t1 - t2)
+        return diff
+
+    ans = -1
+
+    t1 = go(index + 1, first + [index], second)
+
+    if ans == -1 or (t1 != - 1 and ans > t1):
+        ans = t1
+
+    t2 = go(index + 1, first, second + [index])
+
+    if ans == -1 or (t2 != -1 and ans > t2):
+        ans = t2
+
+    return ans
+
+print(go(0, [], []))
