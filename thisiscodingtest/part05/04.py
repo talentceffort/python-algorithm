55bf5554b00365c5b69f694ad86d09009e964438
Jaeik <talentceffort@gmail.com>
2022-02-12 12:12:29 +0900

    feat: 미로탈출

---
+++ thisiscodingtest/part05/04.py
@@ -0,0 +1,46 @@
+from sys import stdin
+from collections import deque
+
+stdin = open("04.txt")
+
+input = stdin.readline
+
+n, m = map(int, input().split())
+
+graph = []
+
+for i in range(n):
+    graph.append(list(map(int, input().rstrip())))
+
+# 상, 하, 좌, 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        # 현재 위치에서 네 방향으로의 위치 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    return graph[n - 1][m - 1]
+
+print(graph)
+bfs(0, 0)
+print(graph)
