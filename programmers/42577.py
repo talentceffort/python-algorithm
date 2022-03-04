26ef82b171d5e940826dbf23ba5f83cb41251b1f
Jaeik <talentceffort@gmail.com>
2022-03-04 20:27:29 +0900

    feat: 해시 전화번호 목록

---
+++ programmers/42577.py
@@ -0,0 +1,9 @@
+# 해쉬
+# 전화번호 목록
+def solution(phoneBook):
+    phoneBook = sorted(phoneBook)
+
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
